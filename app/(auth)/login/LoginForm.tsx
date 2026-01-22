7d7f1008d29f032c3e485e14d1b2d99c58bb2bd5
Fillip Mangia <fillipgms@gmail.com>
2026-01-22 13:51:08 -0300

    login res log

--- app/(auth)/login/LoginForm.tsx
+++ app/(auth)/login/LoginForm.tsx
@@ -27,8 +27,6 @@ const LoginForm = () => {
         const formData = new FormData(e.target as HTMLFormElement);
         const result = await logIn(formData);
 
-        console.log("result from login:", result);
-
         if (result.success) {
             if (result.user) {
                 localStorage.setItem("user", JSON.stringify(result.user));
